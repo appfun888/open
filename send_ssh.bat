a16278daefa119acd3b92b81c1ec287254bfea7e
Iven Chen <670144921@qq.com>
2024-07-02 19:20:20 +0800

    20240702

--- send_ssh.bat
+++ send_ssh.bat
@@ -1,3 +1,3 @@
-scp -P 44485 ./*.db root@104.219.215.236:/www/wwwroot/16200
+scp -P 31396 ./*.db root@107.148.19.153:/www/wwwroot/16200
 scp -P 33829 ./*.db root@156.242.10.233:/www/wwwroot/new.open
 pause
