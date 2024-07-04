fc924882576528b3c4f4afdca837767f0efecc32
bt <bt@atshe.top>
2024-07-04 18:21:57 +0800

    20240704

--- send.sh
+++ send.sh
@@ -3,3 +3,6 @@ git add ./*
 git commit -m "$(date +%Y%m%d)"
 git push
 git pull
+
+scp -P 31396 ./*.db root@107.148.19.153:/www/wwwroot/16200
+scp -P 33829 ./*.db root@156.242.10.233:/www/wwwroot/new.open
