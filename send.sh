70d8418e5e1aaa6f3c7496e783b812ceaa7d6701
bt <bt@atshe.top>
2024-10-17 13:56:17 +0800

    20241017

--- send.sh
+++ send.sh
@@ -7,7 +7,7 @@ git push
 git pull
 
 scp -P 31396 ./*.db root@107.148.19.153:/www/wwwroot/16200
-scp -P 33829 ./*.db root@156.242.10.233:/www/wwwroot/new.open
+# scp -P 33829 ./*.db root@156.242.10.233:/www/wwwroot/new.open
 
 
 # PROCESS=`ls | grep .db`
