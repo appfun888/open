a091976b44046395fe1c0bebb6abf44731ba665a
bt <bt@atshe.top>
2024-11-04 20:40:15 +0800

    20241104

--- send.sh
+++ send.sh
@@ -7,6 +7,8 @@ git push
 git pull
 
 scp -P 31396 ./*.db root@107.148.19.153:/www/wwwroot/16200
+# root@104.219.214.74 -p 12824
+scp -P 12824 ./*.db root@104.219.214.74:/www/wwwroot/open
 # scp -P 33829 ./*.db root@156.242.10.233:/www/wwwroot/new.open
 
 
